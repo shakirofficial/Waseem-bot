6637cdc40d709134799ed90811dd834da253770a
LukeDament <148967386+LukeDament@users.noreply.github.com>
2023-12-28 11:22:44 +0500

    Update main-alive.js

--- plugins/main-alive.js
+++ plugins/main-alive.js
@@ -3,7 +3,7 @@ let handler = async(m, { conn, text, usedPrefix, command }) => {
     // Sound
     let name = m.pushName || conn.getName(m.sender)
     var vn = "https://github.com/Princegds786/PRINCE_BOT/raw/main/Assets/mp3/Audio5.mp3"
-    let url = "https://github.com/Guru322/GURU-BOT"
+    let url = "https://github.com/LukeDament/GURU-BOT"
     let murl = "https://youtu.be/DibiLc17dh0?si=xp9bQ-_frEyDB1-i"
     let img = "https://i.imgur.com/jF3IodN.jpeg"
     let con = { key: { fromMe: false, participant: `${m.sender.split`@`[0]}@s.whatsapp.net`, ...(m.chat ? { remoteJid: '16504228206@s.whatsapp.net' } : {}) }, message: { contactMessage: { displayName: `${name}`, vcard: `BEGIN:VCARD\nVERSION:3.0\nN:;a,;;;\nFN:${name}\nitem1.TEL;waid=${m.sender.split('@')[0]}:${m.sender.split('@')[0]}\nitem1.X-ABLabel:Ponsel\nEND:VCARD`}}}
@@ -19,10 +19,10 @@ let handler = async(m, { conn, text, usedPrefix, command }) => {
         contextInfo: {
           mentionedJid: [m.sender],
           externalAdReply: {
-          title: "I AM ALIVE",
-          body: "PRINCE BOT",
+          title: "_*I AM ALIVE*_",
+          body: "MR HIDDENX",
           thumbnailUrl: img,
-          sourceUrl: 'https://chat.whatsapp.com/FFDazafm9Z5IYthaQecbyN',
+          sourceUrl: 'https://chat.whatsapp.com/F6SfoiNYw2q8sbcQW1lvWs',
           mediaType: 1,
           renderLargerThumbnail: true
           }}
