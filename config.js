625bb0c0762a760c972a736414b934750e43294a
D34d4l0n3 <156603945+D34d4l0n3@users.noreply.github.com>
2024-01-16 13:32:03 +0500

    Update config.js

--- config.js
+++ config.js
@@ -12,15 +12,15 @@ import moment from "moment-timezone"
 
 
 global.owner = [
-  ["923092668108", "Prince", false],
+  ["923196663628", " *ᗪ乇ᐯ|ㄥ ☠︎︎ 乙ㄖ几乇*", true],
   [''], 
   [''] 
 ]
 //💌------------------------------------------💌
 
 
-//💌global.pairingNumber = "" //put your bot number here💌
-global.mods = ['923042205427'] 
+//💌global.pairingNumber = "923196663628" //put your bot number here💌
+global.mods = ['923196663628'] 
 global.prems = ['']
 global.allowed = ['']
 global.keysZens = ['c2459db922', '37CC845916', '6fb0eff124']
@@ -120,16 +120,16 @@ global.developer = 'https://wa.me/message/DCAK67ON3XVOG1' //contact
 
 
 //Sticker WM
-global.botname = '𝘗𝘙𝘐𝘕𝘊𝘌-𝘉𝘖𝘛-𝘔𝘋'
-global.princebot = '🛡️𝘗𝘙𝘐𝘕𝘊𝘌-𝘉𝘖𝘛-𝘔𝘋🛡️'
-global.packname = 'Prince♥️' 
-global.author = 'Prince♥️' 
+global.botname = ' *ᗪ乇ᐯ|ㄥ ☠︎︎ 乙ㄖ几乇*'
+global.princebot = '🛡️ *ᗪ乇ᐯ|ㄥ ☠︎︎ 乙ㄖ几乇*🛡️'
+global.packname = 'Abdulla♥️' 
+global.author = 'Abdullah♥️' 
 global.princeig = 'https://www.instagram.com' 
-global.princegp = 'https://chat.whatsapp.com/GWJkAJSgbv27sGOMLAzMDS'
-global.menuvid = 'https://i.imgur.com/GFAAXqw.mp4'
-global.Princesc = 'https://github.com/PRINCE-GDS/THE-PRINCE-BOT' 
+global.princegp = 'https://wa.me/923196663628'
+global.menuvid = 'https://i.imgur.com/'
+global.Princesc = 'https://github.com/' 
 global.princeyt = 'https://youtube.com/'
-global.Princelog = 'https://i.imgur.com/cUvIv5w.jpeg'
+global.Princelog = 'https://i.imgur.com/'
 global.thumb = fs.readFileSync('./Assets/Prince.png')
 //💌------------------------------------------💌
 
