91cfbbaeeddc8a6efab50981d36239e12a78c18b
CYBER WARRIOR <134825125+Cyberwarrior125@users.noreply.github.com>
2024-02-01 06:09:13 +0500

    Update main-creator.js

--- plugins/main-creator.js
+++ plugins/main-creator.js
@@ -1,16 +1,16 @@
 var handler  = async (m, { conn, command, args, usedPrefix, DevMode }) => {
 let fkontak = { "key": { "participants":"0@s.whatsapp.net", "remoteJid": "status@broadcast", "fromMe": false, "id": "Halo" }, "message": { "contactMessage": { "vcard": `BEGIN:VCARD\nVERSION:3.0\nN:Sy;Bot;;;\nFN:y\nitem1.TEL;waid=${m.sender.split('@')[0]}:${m.sender.split('@')[0]}\nitem1.X-ABLabel:Ponsel\nEND:VCARD` }}, "participant": "0@s.whatsapp.net" }
 let pp = princeImg.getRandom()	
-const cat = `*_🛡️ᴘʀɪɴᴄᴇ ʙᴏᴛ ᴍᴅ🛡️_*
+const cat = `𝐂𝐘𝐁𝐄𝐑_𝐖𝐀𝐑𝐑𝐈𝐎𝐑'
 
 
-*_—◉ ᴘʀɪɴᴄᴇ ʙᴏᴛ ᴏᴡɴᴇʀ 𝟷 wa.me/923092668108_*
+* 𝐂𝐘𝐁𝐄𝐑_𝐖𝐀𝐑𝐑𝐈𝐎𝐑 wa.me/923289091295*
 
-*_—◉ ᴘʀɪɴᴄᴇ ʙᴏᴛ ᴏᴡɴᴇʀ 2 wa.me/92_*
+*𝐂𝐘𝐁𝐄𝐑_𝐖𝐀𝐑𝐑𝐈𝐎𝐑 2 wa.me/92_*
 *---------------------*
 
 
-*_ᴛʜᴇ ᴍᴏᴅᴇʀᴀᴛᴏʀ_*
+*𝐂𝐘𝐁𝐄𝐑_𝐖𝐀𝐑𝐑𝐈𝐎𝐑*
 *${developer}*`
 
 await conn.sendFile(m.chat, pp, 'menuvid', cat, fkontak)
