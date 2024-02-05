ee328a41516199c6d011f1c88bf9313cd10158d2
NIHAL-SER <127925929+NIHAL-SER@users.noreply.github.com>
2024-02-05 14:22:28 +0530

    Update main-creator.js

--- plugins/main-creator.js
+++ plugins/main-creator.js
@@ -1,12 +1,12 @@
 var handler  = async (m, { conn, command, args, usedPrefix, DevMode }) => {
 let fkontak = { "key": { "participants":"0@s.whatsapp.net", "remoteJid": "status@broadcast", "fromMe": false, "id": "Halo" }, "message": { "contactMessage": { "vcard": `BEGIN:VCARD\nVERSION:3.0\nN:Sy;Bot;;;\nFN:y\nitem1.TEL;waid=${m.sender.split('@')[0]}:${m.sender.split('@')[0]}\nitem1.X-ABLabel:Ponsel\nEND:VCARD` }}, "participant": "0@s.whatsapp.net" }
-let pp = princeImg.getRandom()	
-const cat = `*_🛡️ᴘʀɪɴᴄᴇ ʙᴏᴛ ᴍᴅ🛡️_*
+let pp = https://i.imgur.com/4kL2rZ7.jpg()	
+const cat = `*_🛡️ 𝚴𝚰𝚮𝚫𝐋 𝑺𝑬𝑹 🛡️_*
 
 
-*_—◉ ᴘʀɪɴᴄᴇ ʙᴏᴛ ᴏᴡɴᴇʀ 𝟷 wa.me/923092668108_*
+*_—◉ 𝚴𝚰𝚮𝚫𝐋 𝑺𝑬𝑹 ʙᴏᴛ ᴏᴡɴᴇʀ 𝟷 wa.me/917560893006_*
 
-*_—◉ ᴘʀɪɴᴄᴇ ʙᴏᴛ ᴏᴡɴᴇʀ 2 wa.me/92_*
+*_—◉ 𝚴𝚰𝚮𝚫𝐋 𝑺𝑬𝑹 ʙᴏᴛ ᴏᴡɴᴇʀ 2 wa.me/91_*
 *---------------------*
 
 
