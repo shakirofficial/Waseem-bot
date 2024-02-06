a45f7586a7daedf397d4786f1bb288581e007e5b
Itx_manno_05 <149866936+Itxmanno05@users.noreply.github.com>
2024-02-06 17:32:25 +0500

    Update config.js

--- config.js
+++ config.js
@@ -106,7 +106,7 @@ global.gpt6 = fs.readFileSync("./Assets/GPT/gpt6.png")
 
 
 // Randome
-global.princeMenu = [menu.jpg, img2, img6, img7, img8, img9, img13, img14, img15, img16, img17]
+global.princeMenu = [img, img2, img6, img7, img8, img9, img13, img14, img15, img16, img17]
 global.princeImg = [imagen1, imagen2, imagen3, imagen4, imagen5, imagen6, imagen7, imagen8, imagen9, imagen10, imagen11]
 global.princegpt = [gpt1, gpt2, gpt3, gpt4, gpt5, gpt6]
 //💌------------------------------------------💌
