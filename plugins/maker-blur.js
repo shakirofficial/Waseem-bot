0f656ec85ac5ceb9f87accf501040ef1338c0249
Itx_manno_05 <149866936+Itxmanno05@users.noreply.github.com>
2024-02-10 21:05:32 +0500

    Update maker-blur.js

--- plugins/maker-blur.js
+++ plugins/maker-blur.js
@@ -2,7 +2,7 @@ let handler = async (m, { conn, usedprefix }) => {
     let who = m.quoted ? m.quoted.sender : m.mentionedJid && m.mentionedJid[0] ? m.mentionedJid[0] : m.fromMe ? conn.user.jid : m.sender
     conn.sendFile(m.chat, global.API('https://some-random-api.com', '/canvas/misc/blur', {
     avatar: await conn.profilePictureUrl(who, 'image').catch(_ => 'https://telegra.ph/file/24fa902ead26340f3df2c.png'),
-    }), 'hornycard.png', '*[ ✔ ]*', m)
+    }), 'hornycard.png', '*🄼 GRANTED-BY-THE-MANNO-MD*', m)
     }
     handler.help = ['blur','difuminar2']
     handler.tags = ['maker']
