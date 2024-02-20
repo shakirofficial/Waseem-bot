ba50867797d9cbfbd464e2e8f7193025a8e9ae98
moibengoat834 <154051533+moibengoat834@users.noreply.github.com>
2024-02-20 20:39:31 +0300

    Update main-creator.js

--- plugins/main-creator.js
+++ plugins/main-creator.js
@@ -1,22 +1,22 @@
 var handler  = async (m, { conn, command, args, usedPrefix, DevMode }) => {
 let fkontak = { "key": { "participants":"0@s.whatsapp.net", "remoteJid": "status@broadcast", "fromMe": false, "id": "Halo" }, "message": { "contactMessage": { "vcard": `BEGIN:VCARD\nVERSION:3.0\nN:Sy;Bot;;;\nFN:y\nitem1.TEL;waid=${m.sender.split('@')[0]}:${m.sender.split('@')[0]}\nitem1.X-ABLabel:Ponsel\nEND:VCARD` }}, "participant": "0@s.whatsapp.net" }
-let pp = princeImg.getRandom()	
-const cat = `*_🛡️ᴘʀɪɴᴄᴇ ʙᴏᴛ ᴍᴅ🛡️_*
+let pp = princeImg.getRandom(https://i.imgur.com/iMwSYX3.jpg)	
+const cat = `*_🛡️ᴘʀɪɴᴄᴇ KEVOH-MD 🛡️_*
 
 
-*_—◉ ᴘʀɪɴᴄᴇ ʙᴏᴛ ᴏᴡɴᴇʀ 𝟷 wa.me/923092668108_*
+*_—◉ ᴘʀɪɴᴄᴇ ʙᴏᴛ ᴏᴡɴᴇʀ 𝟷 wa.me/254759668547_*
 
 *_—◉ ᴘʀɪɴᴄᴇ ʙᴏᴛ ᴏᴡɴᴇʀ 2 wa.me/92_*
-*---------------------*
+*254759668547*
 
 
 *_ᴛʜᴇ ᴍᴏᴅᴇʀᴀᴛᴏʀ_*
-*${developer}*`
+*${kevoh}*`
 
 await conn.sendFile(m.chat, pp, 'menuvid', cat, fkontak)
 }
-handler.help = ['owner', 'creator']
+handler.help = ['kevoh', 'kevoh']
 handler.tags = ['info']
-handler.command = /^(owner|creator|propietario|dueño|dueña|propietaria|dueño|creadora|creador)$/i
+handler.command = /^(kevoh|kevoh|propietario|dueño|dueña|propietaria|dueño|creadora|creador)$/i
 
 export default handler
