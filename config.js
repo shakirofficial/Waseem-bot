79d4dc26b2c05ba27167a784a6a2f0d9d842e21b
choice2bizzy <132263103+choice2bizzy@users.noreply.github.com>
2024-02-25 13:40:20 +0300

    Update config.js

--- config.js
+++ config.js
@@ -12,7 +12,7 @@ import moment from "moment-timezone"
 
 
 global.owner = [
-  ["923092668108", "Prince", false],
+  ["255693608625", "2bizzy", false],
   [''], 
   [''] 
 ]
@@ -20,7 +20,7 @@ global.owner = [
 
 
 //💌global.pairingNumber = "" //put your bot number here💌
-global.mods = ['923042205427'] 
+global.mods = ['255693608625'] 
 global.prems = ['']
 global.allowed = ['']
 global.keysZens = ['c2459db922', '37CC845916', '6fb0eff124']
