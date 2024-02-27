3db575bd4fc78f769f443672d23c69ac56e55f20
Itx_manno_05 <149866936+Itxmanno05@users.noreply.github.com>
2024-02-28 01:59:08 +0500

    🏅ＴＨＥ-ＭＡＮＮＯ-ＢＯＴ🏅

--- views/index.js
+++ views/index.js
@@ -2,7 +2,7 @@ window.onload = () => {
 let chat = document.querySelector('div.container-fluid')
 function addMsg(obj) {
   let html = document.createElement('span')
-  html.className = 'msg'
+  html.className = 'Msg'
   html.innerHTML = obj
   chat.appendChild(html)
 }
