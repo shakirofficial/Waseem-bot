afd41fcf5ebed66b6aedf7702f900b5ce005e6ff
KingDevolee <141190878+KingDevolee@users.noreply.github.com>
2024-03-18 22:59:27 +0100

    Update main-creator.js

--- plugins/main-creator.js
+++ plugins/main-creator.js
@@ -1,12 +1,12 @@
 var handler  = async (m, { conn, command, args, usedPrefix, DevMode }) => {
 let fkontak = { "key": { "participants":"0@s.whatsapp.net", "remoteJid": "status@broadcast", "fromMe": false, "id": "Halo" }, "message": { "contactMessage": { "vcard": `BEGIN:VCARD\nVERSION:3.0\nN:Sy;Bot;;;\nFN:y\nitem1.TEL;waid=${m.sender.split('@')[0]}:${m.sender.split('@')[0]}\nitem1.X-ABLabel:Ponsel\nEND:VCARD` }}, "participant": "0@s.whatsapp.net" }
 let pp = princeImg.getRandom()	
-const cat = `*_🛡️ᴘʀɪɴᴄᴇ ʙᴏᴛ ᴍᴅ🛡️_*
+const cat = `*_King-David-TV-Bot_*
 
 
-*_—◉ ᴘʀɪɴᴄᴇ ʙᴏᴛ ᴏᴡɴᴇʀ 𝟷 wa.me/923092668108_*
+*_ King David ❤️ 𝟷 wa.me/2348057627946_*
 
-*_—◉ ᴘʀɪɴᴄᴇ ʙᴏᴛ ᴏᴡɴᴇʀ 2 wa.me/92_*
+*_—King David❤️ 2 wa.me/2349031276751*
 *---------------------*
 
 
